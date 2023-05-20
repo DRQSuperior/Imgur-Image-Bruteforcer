14f8ee3c2c7d3144bc0b92469d86a28dba26f283
DRQSuperior <74627000+DRQSuperior@users.noreply.github.com>
2023-05-20 02:14:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import random
 import threading
 import time
@@ -13,17 +12,17 @@ Type = "PNG"
 
 if os.path.exists("logs.log"):
     os.remove("logs.log")
-    open("logs.log", "w+")
-else:
-    open("logs.log", "w+")
+open("logs.log", "w+")
 if not os.path.exists("images"):
     os.mkdir("images")
 
+session = requests.Session()
+lock = threading.Lock()
+
 def log(text, type="log"):
-    logfile = open("logs.log", "a")
-    logstring = "[" + time.strftime("%H:%M:%S") + "] " + text
-    logfile.write(logstring + "\r")
-    logfile.close()
+    with open("logs.log", "a") as logfile:
+        logstring = "[" + time.strftime("%H:%M:%S") + "] " + text
+        logfile.write(logstring + "\r")
 
     if type == "log":
         print("\033[1;37m[\033[1;34mLOG\033[1;37m]\033[1;37m " + text)
@@ -32,63 +31,64 @@ def log(text, type="log"):
     elif type == "success":
         print("\033[1;37m[\033[1;32mSUCCESS\033[1;37m]\033[1;37m " + text)
 
+def update_title():
+    global Valid
+    global Invalid
+    global Type
+
+    while True:
+        os.system("title Imgur Image Brute Forcer by @DRQSuperior - Valid: " + str(Valid) + " - Invalid: " + str(Invalid) + " - Type: " + Type)
+        time.sleep(0.1)
+
 def get_image():
     global Valid
     global Invalid
     global Type
 
     CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    random_string = "".join(random.choice(CHARSET) for i in range(7))
+    random_string = "".join(random.choice(CHARSET) for _ in range(7))
     try:
-        #brute force imgur images
+        # brute force imgur images
         url = "https://i.imgur.com/" + random_string + "." + Type
-        r = requests.get(url, allow_redirects=True)
+        r = session.get(url, allow_redirects=True)
         img = r.content
         if r.url == "https://i.imgur.com/removed.png":
-            log("Invalid image: " + url)
-            Invalid += 1
+            with lock:
+                Invalid += 1
+                update_title()
+            log("Invalid image: " + url, "error")
         else:
-            log("Valid image: " + url)
-            Valid += 1
-            open("images/" + random_string + "." + Type, "wb").write(img)
+            with lock:
+                Valid += 1
+                open("images/" + random_string + "." + Type, "wb").write(img)
+                update_title()
+            log("Valid image: " + url, "success")
             
     except:
         log("Missed image: " + url)
-        Misses += 1
 
 def main():
     global Type
 
-    #print banner in colors saying Imgur Image Brute Forcer by @DRQSuperior
     print("\033[1;37m[\033[1;34mLOG\033[1;37m]\033[1;37m Imgur Image Brute Forcer by \033[1;31m@DRQSuperior\033[1;37m")
 
-    #ask user for type of image
     Type = input("\033[1;37m[\033[1;34mLOG\033[1;37m]\033[1;37m What type of image do you want to brute force? (PNG, JPG, JPEG, GIF, WEBP, CUSTOM) ").upper()
 
-    #if user wants to brute force custom image type
     if Type == "CUSTOM":
         Type = input("\033[1;37m[\033[1;34mLOG\033[1;37m]\033[1;37m What extension do you want to brute force? (Example: PNG) ").upper()
-    elif Type == "JPG":
-        Type = "JPG"
-    elif Type == "JPEG":
-        Type = "JPEG"
-    elif Type == "WEBP":
-        Type = "WEBP"
-    elif Type == "GIF":
-        Type = "GIF"
-    elif Type == "PNG":
-        Type = "PNG"
+    elif Type in ["JPG", "JPEG", "WEBP", "GIF", "PNG"]:
+        Type = Type
     else:
         log("Invalid image type, exiting...", "error")
         sys.exit()
-
-
+    Type = Type.lower()
+    
     while True:
-        for i in range(100):
+        for _ in range(100):
             threading.Thread(target=get_image).start()
 
         try:
-            time.sleep(0.0001)
+            time.sleep(0.1)
         except KeyboardInterrupt:
             log("Keyboard Interrupt detected, exiting...")
             sys.exit()
